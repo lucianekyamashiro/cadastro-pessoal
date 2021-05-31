9b4cc7619872eb59f90c51b573fb8389c0547c60
lucianekyamashiro <luciane_k_y@yahoo.com.br>
2021-05-31 14:58:35 -0300

    Validar cpf

--- js/functions.js
+++ js/functions.js
@@ -15,6 +15,43 @@ function Enviar() {
     }
 }
 
+function validarCPF() {	
+	cpf = cpf.replace(/[^\d]+/g,'');	
+	if(cpf == '') return false;	
+	// Elimina CPFs invalidos conhecidos	
+	if (cpf.length != 11 || 
+		cpf == "00000000000" || 
+		cpf == "11111111111" || 
+		cpf == "22222222222" || 
+		cpf == "33333333333" || 
+		cpf == "44444444444" || 
+		cpf == "55555555555" || 
+		cpf == "66666666666" || 
+		cpf == "77777777777" || 
+		cpf == "88888888888" || 
+		cpf == "99999999999")
+			return false;		
+	// Valida 1o digito	
+	add = 0;	
+	for (i=0; i < 9; i ++)		
+		add += parseInt(cpf.charAt(i)) * (10 - i);	
+		rev = 11 - (add % 11);	
+		if (rev == 10 || rev == 11)		
+			rev = 0;	
+		if (rev != parseInt(cpf.charAt(9)))		
+			alert('Favor corrigir o CPF.');//return false;		
+	// Valida 2o digito	
+	add = 0;	
+	for (i = 0; i < 10; i ++)		
+		add += parseInt(cpf.charAt(i)) * (11 - i);	
+	rev = 11 - (add % 11);	
+	if (rev == 10 || rev == 11)	
+		rev = 0;	
+	if (rev != parseInt(cpf.charAt(10)))
+    alert('Favor corrigir o CPF.');//return false;		
+	return true;   
+}
+
 //adiciona mascara de cep
 function MascaraCep(e){
         if(somenteNumeros(e)==false){
@@ -72,13 +109,6 @@ function ValidaCep(cep){
     exp = /\d{2}\.\d{3}\-\d{3}/
     if(!exp.test(cep.value))
             alert('Numero de Cep Invalido!');               
-}
-
-//valida data
-function ValidaData(data){
-    exp = /\d{2}\/\d{2}\/\d{4}/
-    if(!exp.test(data.value))
-            alert('Data Invalida!');                        
 }
 
 //valida o CPF digitado
